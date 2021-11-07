c8007e04366fe6b8f183d2a08d7cfa3aac0280d7
bruce <brucec@hotmail.com>
2021-11-07 11:39:43 -0800

    pass teasts for calculate rotational speed

--- ex2/encoderReader/EncoderDataHandler.cs
+++ ex2/encoderReader/EncoderDataHandler.cs
@@ -8,21 +8,12 @@ namespace WindowsFormsApp1
 {
     public class EncoderDataHandler
     {
-        private static readonly Double COUNT_PER_REVOLUTION = 100;
-        private static readonly Double SECONDS_BETWEEN_UART = 0.04;
-        private static readonly Double SIXTY_SECONDS_PER_MINUTE = 60;
+        public static readonly Double COUNT_PER_REVOLUTION = 100;
+        public static readonly Double SECONDS_BETWEEN_UART = 0.04;
+        public static readonly Double SIXTY_SECONDS_PER_MINUTE = 60;
 
         public static Double calculateRotationalSpeedHz(EncoderData encoderData) {
-
-            byte[] channelAByteArray =
-            { (byte) encoderData.channelADiffLSB, (byte) encoderData.channelADiffMSB};
-            double channelADiffCount = BitConverter.ToUInt16(channelAByteArray, 0);
-
-            byte[] channelBByteArray =
-                { (byte) encoderData.channelBDiffLSB, (byte) encoderData.channelBDiffMSB};
-            double channelBDiffCount = BitConverter.ToUInt16(channelBByteArray, 0);
-
-            return (channelADiffCount - channelBDiffCount) * (1 / COUNT_PER_REVOLUTION) / SECONDS_BETWEEN_UART;
+            return calculateNetStepChange(encoderData) * (1 / COUNT_PER_REVOLUTION) / SECONDS_BETWEEN_UART;
 
         }
 
---
+++ ex2/encoderReaderTests/EncoderDataHandlerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Tests
+{
+    [TestClass()]
+    public class EncoderDataHandlerTests
+    {
+        [TestMethod()]
+        public void calculateRotationalSpeedHzTest()
+        {
+            EncoderData encoderData = new EncoderData(127, 123, 154, 167);
+            double expectedRotationalSpeedHz = (32635 - 39591) / EncoderDataHandler.COUNT_PER_REVOLUTION / EncoderDataHandler.SECONDS_BETWEEN_UART;
+            Assert.AreEqual(EncoderDataHandler.calculateRotationalSpeedHz(encoderData), expectedRotationalSpeedHz);
+            //0111111101111011 - 1001101010100111
+        }
+    }
+}
